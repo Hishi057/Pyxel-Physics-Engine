bba2e776b8f07612b694084e8e7411f31f08e547
Hishi057 <Hiratsuka512@icloud.com>
2026-01-08 17:18:26 +0900

    You can set up a pyxel project more easily

--- examples/0_gravity.py
+++ examples/0_gravity.py
@@ -5,14 +5,14 @@ class Ball(pyxphys.GameObject):
     color : int = 6 # ボールの色
     radius : int = 10 # ボールの半径
 
-    def __init__(self, world):
-        super().__init__(world)
+    def __init__(self):
+        super().__init__()
         self.name = "ball"
         self.x = pyxel.rndi(0, 199)
-        self.y = 0
+        self.y = 100
         angle = pyxel.rndi(30, 150)
-        self.vx = pyxel.cos(angle)
-        self.vy = pyxel.sin(angle)
+        self.vx = pyxel.cos(angle) * 3 + 3
+        self.vy = -10
     
     def update(self):
         if self.x < self.radius:
@@ -21,17 +21,22 @@ class Ball(pyxphys.GameObject):
         if app.screen_x - self.radius < self.x:
             self.x = app.screen_x - self.radius
             self.vx *= -1
+        
+
+        limit_y = app.screen_y - self.radius # 床の位置
         if app.screen_y - self.radius < self.y:
-            self.y = app.screen_y - self.radius 
-            self.vy *= -1
+            overlap = self.y - limit_y #
+            self.y = limit_y - overlap # 床にめり込んだ分だけ戻す
+            self.vy *= -1.0
 
     def draw(self):
         pyxel.circ(self.x, self.y, self.radius, self.color)
 
-app = pyxphys.App(200,200)
-world = pyxphys.World(gravity = 0.9)
-Ball(world)
+# 初期設定
+app = pyxphys.App(200,200) # アプリ本体
+world = pyxphys.World(gravity = 0.9) # アプリの中における世界
+app.add_world(world) # ゲーム本体に、世界を追加
 
-pyxel.run(world.update, world.draw)
+world.add_object(Ball()) # "world"という世界にBallオブジェクトを追加
 
-
+app.run() # アプリを実行
--- src/pyxphys/core.py
+++ src/pyxphys/core.py
@@ -5,10 +5,29 @@ from typing import List
 class App:
     screen_x : int
     screen_y : int
+    worlds : list[World]
+
     def __init__(self,screen_x,screen_y):
         self.screen_x = screen_x
         self.screen_y = screen_y
         pyxel.init(screen_x, screen_y)
+        self.worlds = []
+    
+    def add_world(self, world : World):
+        self.worlds.append(world)
+    
+    def run(self):
+        pyxel.run(self.update, self.draw)
+    
+    def update(self):
+        for w in self.worlds: 
+            w.update_physics()
+            
+    def draw(self):
+        pyxel.cls(7)
+        for w in self.worlds:
+            for o in w.objects:
+                o.draw()
 
 class World:
     objects : list[GameObject]
@@ -16,32 +35,32 @@ class World:
     def __init__(self, gravity : float = 0):
         self.objects = []
         self.gravity = gravity
-    
-    def update(self):
-        # is_alive = False の オブジェクトを削除する処理
+
+    def update_physics(self):
+        # オブジェクトを削除する処理
         new_objects = []
         for o in self.objects:
             if o.is_alive:
                 new_objects.append(o)
         self.objects = new_objects
-            
-        # オブジェクトごとの処理
+
+        # 物理演算
         for o in self.objects:
-            o.move()
+            o.vx += o.ax
+            o.vy += o.ay + self.gravity
+            o.x += o.vx
+            o.y += o.vy
             o.update()
-    
+        
         # 衝突の処理 
         for o1 in self.objects:
             for o2 in self.objects:
                 o1.collide(o2)
-    
-    def draw(self):
-        pyxel.cls(7)
-        for o in self.objects:
-            o.draw()
+
+    def add_object(self, object : GameObject):
+        self.objects.append(object)
 
 class GameObject:
-    world : World
     name : str
     is_alive : bool # 消去したければここをFalseにする
     x : float
@@ -51,7 +70,6 @@ class GameObject:
     tags : list[str]
 
     def __init__(self, 
-                 world : World,
                  name : str = "",
                  x : float = 0,
                  y : float = 0,
@@ -60,8 +78,6 @@ class GameObject:
                  ax : float = 0,
                  ay : float = 0,
                  ):
-        self.world = world
-        world.objects.append(self)
 
         # 変数の設定
         self.is_alive = True
@@ -73,13 +89,7 @@ class GameObject:
         self.ax = ax
         self.ay = ay
         self.tags = []
-
-    
-    def move(self):
-        self.vx += self.ax
-        self.vy += self.ay + self.world.gravity
-        self.x += self.vx
-        self.y += self.vy
+        return self
 
     def update(self):
         pass
